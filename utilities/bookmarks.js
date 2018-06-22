7b0cb389c2c9a170429b1b5c659547440e10848a
Michael Lee <michaelsoolee@gmail.com>
2018-06-22 16:45:45 -0400

    Only add valid lines

--- utilities/bookmarks.js
+++ utilities/bookmarks.js
@@ -1,8 +1,10 @@
 function list(bookmarks, cb) {
   const allBookmarks = bookmarks.split(/\r\n|\n/)
   for (let i = 0; i < allBookmarks.length; i++) {
-    let tempBookmark = allBookmarks[i].split(',')
-    allBookmarks[i] = { title: tempBookmark[0], pageOrUrl: tempBookmark[1] }
+    if (allBookmarks[i]) {
+      let tempBookmark = allBookmarks[i].split(',')
+      allBookmarks[i] = { title: tempBookmark[0], pageOrUrl: tempBookmark[1] }
+    }
   }
   cb(allBookmarks)
 }
